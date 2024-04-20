743cd8d94bcd47386b99a6d665a0b39f6cd7cc00
chris rhodes <chrisxrhodes@gmail.com>
2024-04-20 08:49:38 -0700

    corrected logo on small screens

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,8 +2,6 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
 
-
-
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isSolidBg, setIsSolidBg] = useState(false);
@@ -30,40 +28,34 @@ const Navbar = () => {
 
   return (
     <nav className={`${styles.navContainer} ${isSolidBg ? styles.solidBg : ""}`}>
-    
-  <div className={styles.navContainer}>
-    <div className={styles.logo}>
-          <a href="#">
-            <img src="/img/whitelogo.png" alt="Logo" className="logoImg" />
-          </a>
-        </div>
-        <div className={`${styles.navLinks} ${isOpen ? styles.open : ""}`}>
-          <Link href="#About">
-            <span className="dot"></span>{" "}
-            <span className="txt">About</span>
-          </Link>
-          <Link href="#TrainingProgram">
-            <span className="dot"></span>{" "}
-            <span className="txt">Programs</span>
-          </Link>
- 
-        </div>
-        <div className={styles.socialLinks}>
-          <a href="https://twitter.com/enmotionfit" className="socialLink">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="https://www.facebook.com/profile.php?id=61558229676688" className="socialLink">
-            <i className="fab fa-facebook"></i>
-          </a>
-          <a href="https://twitter.com/enmotionfit" className="socialLink">
-            <i className="fa-brands fa-x"></i>
-          </a>
-        </div>
-        <div className={styles.hamburger} onClick={toggleMenu}>
-      <div className={styles.line}></div>
-      <div className={styles.line}></div>
-      <div className={styles.line}></div>
-</div>
+      <div className={styles.logo}>
+        <a href="#">
+          <img src="/img/whitelogo.png" alt="Logo" className={styles.logoImg} />
+        </a>
+      </div>
+      <div className={`${styles.navLinks} ${isOpen ? styles.open : ""}`}>
+        <Link href="#About">
+          <span className="dot"></span> <span className="txt">About</span>
+        </Link>
+        <Link href="#TrainingProgram">
+          <span className="dot"></span> <span className="txt">Programs</span>
+        </Link>
+      </div>
+      <div className={`${styles.socialLinks} ${isOpen ? styles.open : ""}`}>
+        <a href="https://twitter.com/enmotionfit" className="socialLink">
+          <i className="fab fa-instagram"></i>
+        </a>
+        <a href="https://www.facebook.com/profile.php?id=61558229676688" className="socialLink">
+          <i className="fab fa-facebook"></i>
+        </a>
+        <a href="https://twitter.com/enmotionfit" className="socialLink">
+          <i className="fa-brands fa-x"></i>
+        </a>
+      </div>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
       </div>
     </nav>
   );
